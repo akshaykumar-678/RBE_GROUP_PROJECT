30b0dc768dbc16592373efa23cc5fea5156bf46d
ATHARVA MAHINDRAKAR <53110598+atharva-9@users.noreply.github.com>
2021-11-15 21:21:28 -0500

    Forward Kinematics is working
    Publisher needs to be updated

--- scara_command/src/sub.py
+++ scara_command/src/sub.py
@@ -9,7 +9,7 @@ import numpy as np
 import math as m
 from math import sin, cos, sqrt
 global pub
-pub=rospy.Publisher("FK",Float64MultiArray,queue_size=10)
+
 
 def callback(position):
     q1 = position.position[0]
@@ -30,14 +30,15 @@ def forw_kine(position):
 	t2 = np.matrix([[cos(q2), sin(q2), 0, sin(q2)], [sin(q2), cos(q2), 0, cos(q2)], [0, 0, 1, 0], [0, 0, 0, 1]])
 	t3 = np.matrix([[cos(q3), -sin(q3), 0, cos(q3)], [sin(q3),cos(q3), 0, sin(q3)], [0, 0, 1, 0], [0, 0, 0, 1]])
 	kine = np.matmul(t1, t2, t3)
-	pub.publish(kine)
 	#print("Kinematics are ",kine,"\n")
+	pub.publish(kine)
 	return kine
 
 def listener():
     rospy.init_node('FK', anonymous=True)
+    pub=rospy.Publisher("FK",Float64MultiArray,queue_size=10)
     sub=rospy.Subscriber("/scara/joint_states", JointState, forw_kine)
-    #pub=rospy.Publisher("FK",Float64MultiArray,queue_size=10)
+
     rate=rospy.Rate(10)
     rospy.spin()
 
