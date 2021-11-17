ce02253c396645f5142a5dbc321b5565cb728eff
ATHARVA MAHINDRAKAR <53110598+atharva-9@users.noreply.github.com>
2021-11-17 00:57:27 -0500

    Forward Kinematics
    Pub not working

--- scara_command/scripts/sub.py
+++ scara_command/scripts/sub.py
@@ -1,16 +1,16 @@
 
 
 #!/usr/bin/env python
+from os import F_OK
 import rospy
 from geometry_msgs.msg import Pose
-from std_msgs.msg import Float64MultiArray 
+from std_msgs.msg import Float32MultiArray 
+
 from sensor_msgs.msg import JointState
 import numpy as np
 import math as m
-from math import sin, cos, sqrt
-global pub
-
-
+from math import sin, cos, sqrt,atan2
+FK=[]
 def callback(position):
     q1 = position.position[0]
     q2 = position.position[1]
@@ -26,21 +26,25 @@ def forw_kine(position):
 	q2 = position.position[1]
 	q3 = position.position[2]
 
-	t1 = np.matrix([[cos(q1), 0, sin(q1), 0], [sin(q1), 0, -cos(q1), 0], [0, 1, 0, 0], [0, 0, 0, 1]])
-	t2 = np.matrix([[cos(q2), sin(q2), 0, sin(q2)], [sin(q2), cos(q2), 0, cos(q2)], [0, 0, 1, 0], [0, 0, 0, 1]])
-	t3 = np.matrix([[cos(q3), -sin(q3), 0, cos(q3)], [sin(q3),cos(q3), 0, sin(q3)], [0, 0, 1, 0], [0, 0, 0, 1]])
-	kine = np.matmul(t1, t2, t3)
-	#print("Kinematics are ",kine,"\n")
-	pub.publish(kine)
-	return kine
-
+	t1 = np.matrix([[cos(q1+q2), -sin(q1+q2), 0,2*cos(q1)+2*cos(q1+q2)], [sin(q1+q2), cos(q1+q2), 0, 2*sin(q1)+2*sin(q1+q2)], [0, 0, -1, 2-q3], [0, 0, 0, 1]])
+	x=t1[0,3]
+	y=t1[1,3]
+	z=t1[2,3]
+	roll = atan2(t1[2, 1], t1[2, 2])
+	pitch = atan2((-1 * t1[2, 0]), sqrt(pow(t1[2, 1], 2) + pow(t1[2, 2], 2)))
+	yaw = atan2(t1[1, 0], t1[0, 0])
+	FK=[x,y,z,roll,pitch,yaw]
+	#print (type(FK))
+	return FK
 def listener():
     rospy.init_node('FK', anonymous=True)
-    pub=rospy.Publisher("/scara/forward_kinematics",Float64MultiArray,queue_size=10)
+    pub=rospy.Publisher("FK",Float32MultiArray,queue_size=10)
     sub=rospy.Subscriber("/scara/joint_states", JointState, forw_kine)
-
+    print(FK)
+    #pub.publish(FK)
     rate=rospy.Rate(10)
     rospy.spin()
+    
 
 
 if __name__ == '__main__':
